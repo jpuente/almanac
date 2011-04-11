665827e6520df57b16d949dc20634057e2a1f33a
jpuente <jpuente@137f3855-912b-0410-af34-a56dcd99bfab>
2011-04-11 10:00:50 +0000

    fixed test program

--- ephemeris/trunk/test/src/ephemeris-test.adb
+++ ephemeris/trunk/test/src/ephemeris-test.adb
@@ -1,3 +1,4 @@
+--  $Id$:
 -----------------------------------------------------------------------
 -- Ephemeris - Ada 2005 utilities for the JPL ephemerides.           --
 --                                                                   --
@@ -25,7 +26,7 @@
 with Ephemeris.Generic_State_Functions;
 
 with Ada.Numerics,
-     Ada.Numerics.Generic_Elementary_Functions,
+--     Ada.Numerics.Generic_Elementary_Functions,
      Ada.Numerics.Generic_Real_Arrays;
 
 with Ada.Text_IO;
@@ -36,128 +37,135 @@ with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
 
 procedure Ephemeris.Test is
 
-  package Real_Functions is
-     new Ada.Numerics.Generic_Elementary_Functions (Real);
+--   package Real_Functions is
+--       new Ada.Numerics.Generic_Elementary_Functions (Real);
    package Real_Arrays is
      new Ada.Numerics.Generic_Real_Arrays (Real);
    package Solar_System is
      new Ephemeris.Generic_State_Functions (Real,
-                                            Real_Functions,
+                                            --  Real_Functions,
                                             Real_Arrays,
                                             Ephemeris_Number);
    use Solar_System;
 
-   package Real_IO    is new Ada.Text_IO.Float_IO(Real);
-   package Integer_IO is new Ada.Text_IO.Integer_IO(Integer);
+   package Real_IO    is new Ada.Text_IO.Float_IO (Real);
+   package Integer_IO is new Ada.Text_IO.Integer_IO (Integer);
    use Ada.Text_IO, Real_IO, Integer_IO;
 
    Test_File_Name : Unbounded_String := Null_Unbounded_String;
    Data_File_Name : Unbounded_String := Null_Unbounded_String;
    Test_File      : File_Type;
 
-   Ephemeris_Name : String := JPL_Ephemeris'Image(Ephemeris_Number);
+   Ephemeris_Name : constant String := JPL_Ephemeris'Image (Ephemeris_Number);
 
-   Tag    : String(1..3); -- test file tags
-   Eph_No : String(1..3); -- ephemeris number in test file records (e.g. "200")
-   Date   : String(1..12);-- calendar date
-   JED    : Real;         -- Julian Ephemeris date
-   Target : Natural;      -- Target object (1..11)
-   Center : Natural;      -- Center object (1..11)
-   Coordinate : Natural;  -- Coordinate number (1..6), position comes first
-   XI,XE  : Real;         -- Coordinate value
-   Diff   : Real;         -- difference
+   Tag    : String (1 .. 3);  -- test file tags
+   Eph_No : String (1 .. 3);  -- ephemeris number in test file records
+   Date   : String (1 .. 12); -- calendar date
+   JED    : Real;             -- Julian Ephemeris date
+   Target : Natural;          -- Target object (1..11)
+   Center : Natural;          -- Center object (1..11)
+   Coordinate : Natural;      -- Coordinate number (1..6), position comes first
+   XI, XE  : Real;            -- Coordinate value
+   Diff   : Real;             -- difference
 
-   XT,XC  : State;        -- state of target and center
+   XT, XC  : State;           -- state of target and center
 
    Line   : Natural  := 0;
-   NOUT   : constant := 10; -- ouput results every NOUT lines
+   NOUT   : constant := 10;   -- ouput results every NOUT lines
+   OK     : Boolean  := True; -- global result of the test
 
 begin
-   -- Open data files
+   --  Open data files
    if Argument_Count > 0 then
-      Test_File_Name := To_Unbounded_String(Full_Name(Argument(1)));
+      Test_File_Name := To_Unbounded_String (Full_Name (Argument (1)));
    else
       Put_Line ("-- Usage: test_ephemeris test_file [data_file]");
-      Set_Exit_Status(Failure);
+      Set_Exit_Status (Failure);
       return;
    end if;
 
    if Argument_Count > 1 then
-      Data_File_Name := To_Unbounded_String(Full_Name(Argument(2)));
+      Data_File_Name := To_Unbounded_String (Full_Name (Argument (2)));
    elsif Ada.Environment_Variables.Exists ("EPHEMERIS") then
-      Data_File_Name := To_Unbounded_String(Value("EPHEMERIS"));
+      Data_File_Name := To_Unbounded_String (Value ("EPHEMERIS"));
    else
-      Data_File_Name := To_Unbounded_String(Compose("data",Ephemeris_Name));
+      Data_File_Name := To_Unbounded_String (Compose ("data", Ephemeris_Name));
    end if;
 
-   Open(Test_File, In_File, To_String(Test_File_Name));
-   Open_Data(To_String(Data_File_Name));
+   Open (Test_File, In_File, To_String (Test_File_Name));
+   Open_Data (To_String (Data_File_Name));
 
-   -- Write header and start test
-   Put("Test JPL Ephemeris " & Ephemeris_Name);
+   --  Write header and start test
+   Put ("*** Test JPL Ephemeris " & Ephemeris_Name & " ***");
    New_Line;
 
-   Put_Line(" line -- jed --   t#   c#   x#   --- jpl value ---"&
+   Put_Line (" line -- jed --   t#   c#   x#   --- jpl value ---"&
             "  --- user value ---    -- difference --");
    loop
-      Get(Test_File,Tag); Skip_Line(Test_File);
+      Get (Test_File, Tag); Skip_Line (Test_File);
       exit when Tag = "EOT";
    end loop;
 
-   -- Execute test
+   --  Execute test
 
-   while not End_Of_File(Test_File) loop
-      -- get test data
-      Get(Test_File,Eph_No);
+   while not End_Of_File (Test_File) loop
+      --  get test data
+      Get (Test_File, Eph_No);
       if "DE"&Eph_No /= Ephemeris_Name then
          raise Ephemeris_Error
            with "Wrong ephemeris number";
       end if;
 
-      Get(Test_File,Date);
-      Get(Test_File,JED);
-      Get(Test_File,Target);
-      Get(Test_File,Center);
-      Get(Test_File,Coordinate);
-      Get(Test_File,XI);
-      Skip_Line(Test_File);
+      Get (Test_File, Date);
+      Get (Test_File, JED);
+      Get (Test_File, Target);
+      Get (Test_File, Center);
+      Get (Test_File, Coordinate);
+      Get (Test_File, XI);
+      Skip_Line (Test_File);
 
-      -- check data
+      --  check data
       if JED >= Start_Date and JED <= End_Date
-        and Target in 1..11 and Center in 1..11 then
-         XT := Barycentric_State(Object'Val(Target-1),JED);
-         XC := Barycentric_State(Object'Val(Center-1),JED);
-         if Coordinate in 1..3 then    -- position component
-            XE := XT.Position(Coordinate) - XC.Position(Coordinate);
-         elsif Coordinate in 4..6 then -- velocity component
-            XE := XT.Velocity(Coordinate-3) - XC.Velocity(Coordinate-3);
+        and Target in 1 .. 11 and Center in 1 .. 11 then
+         XT := Barycentric_State (Object'Val (Target - 1), JED);
+         XC := Barycentric_State (Object'Val (Center - 1), JED);
+         if Coordinate in 1 .. 3 then    -- position component
+            XE := XT.Position (Coordinate) - XC.Position (Coordinate);
+         elsif Coordinate in 4 .. 6 then -- velocity component
+            XE := XT.Velocity (Coordinate - 3) - XC.Velocity (Coordinate - 3);
          else
             raise Ephemeris_Error
               with "Bad coordinate number";
          end if;
-         Diff := abs(XI-XE);
-         -- write data
-         Line := Line +1;
-         if DIFF >= 1.0E-13 then
-            Put_Line("*** Warning : difference > 1.0E-13 in next line ***");
-            Put(Line,5); Put(JED,8,1,0);
-            Put(Target,5); Put(Center,5); Put(Coordinate,5);
-            Put(XI,13,6,0);Put(XE,13,6,0);Put(Diff,13,6,0);
+         Diff := abs (XI - XE);
+         --  write data
+         Line := Line + 1;
+         if Diff >= 1.0E-13 then
+            OK := False;
+            Put_Line ("*** Warning : difference > 1.0E-13 in next line ***");
+            Put (Line, 5); Put (JED, 8, 1, 0);
+            Put (Target, 5); Put (Center, 5); Put (Coordinate, 5);
+            Put (XI, 13, 6, 0); Put (XE, 13, 6, 0); Put (Diff, 13, 6, 0);
             New_Line;
          end if;
          if Line mod NOUT = 0 then
-            Put(Line,5); Put(JED,8,1,0);
-            Put(Target,5); Put(Center,5); Put(Coordinate,5);
-            Put(XI,13,6,0);Put(XE,13,6,0);Put(Diff,13,6,0);
+            Put (Line, 5); Put (JED, 8, 1, 0);
+            Put (Target, 5); Put (Center, 5); Put (Coordinate, 5);
+            Put (XI, 13, 6, 0); Put (XE, 13, 6, 0); Put (Diff, 13, 6, 0);
             New_Line;
          end if;
       end if;
    end loop;
 
-   -- Close files
+   --  Close files
    Close_Data;
-   Close(Test_File);
+   Close (Test_File);
 
-   Put_Line("End of test");
+   if (OK) then
+      Put_Line ("TEST SUCEEDED");
+   else
+      Put_Line ("TEST FAILED -- see details above");
+   end if;
+   Put_Line ("*** End of test *** ");
 
 end Ephemeris.Test;
