4e2452bc311f229ca175bee49633149deaa16200
Juan A. de la Puente <juan.de.la.puente@upm.es>
2023-03-29 10:24:57 +0200

    clean up

--- ephemeris/tags/release-1.0/test/test_ephemeris.ads
+++
@@ -1,4 +0,0 @@
-with Ephemeris, Ephemeris.Test;
-use Ephemeris;
-procedure Test_Ephemeris is
-  new Ephemeris.Test(Long_Long_Float,DE200);
--- ephemeris/tags/release-1.0/utilities/create_ephemeris.ads
+++
@@ -1,4 +0,0 @@
-with Ephemeris, Ephemeris.Create;
-use Ephemeris;
-procedure Create_Ephemeris is
-  new Ephemeris.Create(Long_Long_Float,DE200);
